6db059242eb8000a231dae01b823a390673a9ae8
Студент <top.kambeika@gmail.com>
2025-09-22 14:01:28 +0300

    che to sdelal

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введи натуральное число N");
+            int N = int.Parse(Console.ReadLine());
+            int number = 201;
+            while (number % N != 0)
+            {
+                number++;
+            }
+            Console.WriteLine($"минимальное число которое делится на {N}: {number}");
+            Console.ReadLine();
+        }
+    }
+}
