9ada7e3f5541250dbe1fada9751bc4dcf7268ef7
Студент <top.kambeika@gmail.com>
2025-09-23 15:26:52 +0300

    добавлен класс

--- Class1.cs
+++ Class1.cs
@@ -4,5 +4,19 @@ public class Class1
 {
 	public Class1()
 	{
-	}
+        {
+            Console.Write("Введите натуральное число n:");
+            int n = int.Parse(Console.ReadLine());
+            for (int x = 201; ; x++)
+            {
+                if (x % n == 0)
+                {
+                    Console.WriteLine(x);
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
 }
+}
+
